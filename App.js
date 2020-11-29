a8b362652727b03b7b168647b8facbf77c404bbe
eraldoakosoft <eraldo450518@outlook.com>
2020-11-28 22:25:45 -0300

    corrigido um bug

--- App.js
+++ App.js
@@ -1,5 +1,4 @@
 import React from 'react'
-
 import { NavigationContainer } from '@react-navigation/native'
 import MainStack from './src/stacks/MainStack'
 
--- src/stacks/MainStack.js
+++ src/stacks/MainStack.js
@@ -1,4 +1,4 @@
-import react from 'react'
+import React from 'react'
 import { createStackNavigator } from '@react-navigation/stack'
 
 import Preload from '../screens/Preload'
@@ -8,7 +8,12 @@ import SignUp from '../screens/SignUp'
 const Stack = createStackNavigator()
 
 export default () => (
-    <Stack.Navigator>
+    <Stack.Navigator 
+        initialRouteName="Preload"
+        screenOptions={{
+            headerShown: false
+        }}
+    >
         <Stack.Screen name="Preload" component={Preload} />
         <Stack.Screen name="SignIn" component={SignIn} />
         <Stack.Screen name="SignUp" component={SignUp} />
