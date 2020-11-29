1af336da8c50e55759f3b2cafc93adcf22ace75d
eraldoakosoft <eraldo450518@outlook.com>
2020-11-28 23:48:21 -0300

    Adicionado a tela de lognin

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import MainStack from './src/stacks/MainStack'
+import UserContextProvider from './src/contexts/UserContext'
 
 
 export default () => {
-  return(
-    <NavigationContainer>
-      <MainStack></MainStack>
-    </NavigationContainer>
+  return (
+    <UserContextProvider>
+      <NavigationContainer>
+        <MainStack></MainStack>
+      </NavigationContainer>
+    </UserContextProvider>
   )
 }
--- src/screens/SignIn/index.js
+++ src/screens/SignIn/index.js
@@ -1,11 +1,33 @@
 import React from 'react'
 import { Text } from 'react-native'
-import { Container } from './styles'
+import { 
+    Container,
+    InputArea,
+    CustomButton,
+    CustomButtonText,
+    SignMessageButton,
+    SignMessageButtonText,
+    SignMessageButtonTextBold
+} from './styles'
+import BarberLogo from '../../assets/barber.svg'
 
 export default () => {
     return(
         <Container>
-            <Text>SignIn</Text>
+            <BarberLogo width="100%" height="160" />
+
+
+            <InputArea>
+                
+                <CustomButton>
+                    <CustomButtonText>LOGIN</CustomButtonText>
+                </CustomButton>
+            </InputArea>
+            <SignMessageButton>
+                <SignMessageButtonText>Ainda não tem uma conta? </SignMessageButtonText>
+                <SignMessageButtonTextBold>Cadastre-se</SignMessageButtonTextBold>
+            </SignMessageButton>
+
         </Container>
     )
 }
