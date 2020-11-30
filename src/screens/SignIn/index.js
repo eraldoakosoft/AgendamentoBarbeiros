1afd4b206b7ac92d349c8ec64aca32e96ff23955
eraldoakosoft <eraldo450518@outlook.com>
2020-11-29 21:34:59 -0300

    finalizado a tela de lognin

--- src/screens/SignIn/index.js
+++ src/screens/SignIn/index.js
@@ -1,6 +1,6 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { Text } from 'react-native'
-import { 
+import {
     Container,
     InputArea,
     CustomButton,
@@ -9,21 +9,46 @@ import {
     SignMessageButtonText,
     SignMessageButtonTextBold
 } from './styles'
+
+
 import BarberLogo from '../../assets/barber.svg'
+import EmailIcon from '../../assets/email.svg'
+import LockIcon from '../../assets/lock.svg'
+
+
+import SignInput from '../../components/SignInput'
 
 export default () => {
-    return(
+
+    const [emailField, setEmailField] = useState('');
+    const [passwordField, setPasswordField] = useState('');
+
+    const handleMessageButtonClick = () => {
+
+    }
+
+    const handleSignClick = () => {
+        
+    }
+
+    return (
         <Container>
             <BarberLogo width="100%" height="160" />
 
-
             <InputArea>
-                
-                <CustomButton>
+
+                <SignInput IconSvg={EmailIcon} placeholder="Digite o seu Email" value={emailField} onChangeText={ t => setEmailField(t)  } >
+
+                </SignInput>
+                <SignInput IconSvg={LockIcon} placeholder="Digite o sua senha" value={passwordField} onChangeText={ t => setPasswordField(t)  } password={true} >
+
+                </SignInput>
+
+                <CustomButton onPress={handleSignClick} >
                     <CustomButtonText>LOGIN</CustomButtonText>
                 </CustomButton>
             </InputArea>
-            <SignMessageButton>
+            <SignMessageButton onPress={handleMessageButtonClick} >
                 <SignMessageButtonText>Ainda não tem uma conta? </SignMessageButtonText>
                 <SignMessageButtonTextBold>Cadastre-se</SignMessageButtonTextBold>
             </SignMessageButton>
